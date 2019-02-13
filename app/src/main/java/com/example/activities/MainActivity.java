23fee1df2c362b5550fe3ed7983f736ff99c2441
luckyboy66 <luckyboy66@mail.ru>
2019-02-14 01:23:20 +0500

    done

--- app/src/main/java/com/example/activities/MainActivity.java
+++ app/src/main/java/com/example/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.activities;
 
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -26,9 +30,17 @@ public class MainActivity extends AppCompatActivity {
 
         if(requestCode == SIMPLE_REQUEST_CODE){
             ArrayList<String> names = data.getStringArrayListExtra(Extras.CONTACTS);
-            for(String name : names){
-                Log.d("1", name);
+            final LinearLayout rl = findViewById(R.id.MainLayout);
+            rl.addView(createTextView("Контакты"));
+            for(String name : names) {
+                rl.addView(createTextView(String.format("Имя контакта: %s", name)));
             }
         }
+    }
+
+    private TextView createTextView(final String text){
+        TextView mainText = new TextView(this);
+        mainText.setText(text);
+        return mainText;
     }
 }
--- app/src/main/java/com/example/activities/SecondaryActivity.java
+++ app/src/main/java/com/example/activities/SecondaryActivity.java
@@ -1,13 +1,16 @@
 package com.example.activities;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.example.activities.services.ContactsIntentService;
 
@@ -15,14 +18,16 @@ import java.util.List;
 
 public class SecondaryActivity extends AppCompatActivity {
 
+    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secondary);
 
-        ContactsIntentService.startGetContactAction(this);
-
         LocalBroadcastManager.getInstance(this).registerReceiver(contactsReceiver, new IntentFilter(Actions.GET_CONTACTS));
+
+        requestContacts();
     }
 
     private BroadcastReceiver contactsReceiver = new BroadcastReceiver() {
@@ -33,5 +38,24 @@ public class SecondaryActivity extends AppCompatActivity {
         }
     };
 
+    private void requestContacts(){
+        if (checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+        } else {
+            ContactsIntentService.startGetContactAction(this);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                requestContacts();
+            } else {
+                Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 
 }
